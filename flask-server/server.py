fe2318f9c9cacc5b722e249b174bc1b0404225e9
jrebollar <jrebollar@cpp.edu>
2023-02-27 19:08:40 -0800

    Working on login/register page.

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, jsonify, request, abort
+from flask_bcrypt import Bcrypt
+from config import ApplicationConfig
+from models import db, User
 
 app = Flask(__name__)
+app.config.from_object(ApplicationConfig)
 
-@app.route("/members")
-def members():
-    return{"members": ["Member1", "Member2", "Member3"]}
+bcrypt = Bcrypt(app)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/register")
+def register_user():
+    email = request.json["email"]
+    password = request.json["password"]
+
+    user_exists = User.query.filer_by(email=email).first() is not None
+
+    if user_exists:
+        abort(409)
+    
+    hashed_password = bcrypt.generate_password_hash(password)
+    new_user = User(email=email, hashed_password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return jsonify({
+        "id": new_user.id,
+        "email": new_user.email
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
